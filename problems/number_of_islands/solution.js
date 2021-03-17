c2ae368b37cd7dcc20ab9a77dea967bc55f84222
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-03-17 19:56:38 +0000

    Sync LeetCode submission - Number of Islands (javascript)

---
+++ problems/number_of_islands/solution.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+function dfs(grid, i, j) {
+    
+    if (i >= grid.length || i < 0 || j < 0 || j >= grid[0].length || grid[i] && grid[i][j] === '0')
+        return;
+    grid[i][j] = '0';
+    dfs(grid, i, j + 1);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i - 1 , j);
+}
+
+function countIslands(grid) {
+    let count = 0;
+    for(let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === '1') {
+                count++;
+                dfs(grid, i, j)
+            }
+        }
+    }
+    return count;
+}
+
+var numIslands = function(grid) {
+    let islandCount = {
+        count: 0
+    }
+    return countIslands(grid);
+    
+};
+
+
+
