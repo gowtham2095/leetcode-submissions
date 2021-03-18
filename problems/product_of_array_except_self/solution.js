78d47c6f9866a63c0ac822794af2138ec6a8d5fb
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-03-18 16:42:57 +0000

    Sync LeetCode submission - Product of Array Except Self (javascript)

--- problems/product_of_array_except_self/solution.js
+++ problems/product_of_array_except_self/solution.js
@@ -2,23 +2,31 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+function Node(val) {
+    this.val = val;
+    this.next = null;
+}
+
 var productExceptSelf = function(nums) {
 
-    let cumulativeValue = 1;
-    let output = [];
-    if (nums.length) {
-        output.push(1);
-    }
+    let leftArray = [];
+    let rightArray = [];
+    
+    leftArray.push(1);
+    let multiplicationFactor = leftArray[0];
     for (let i = 1; i < nums.length; i++) {
-        cumulativeValue = cumulativeValue * nums[i - 1];
-        output[i] = cumulativeValue;
+           leftArray[i] = nums[i - 1] * multiplicationFactor;
+           multiplicationFactor = leftArray[i];
     }
-    cumulativeValue = 1;
-    for (let i = nums.length - 2; i >= 0; i--) {
-        cumulativeValue = cumulativeValue * nums[i + 1];
-        output[i] = output[i] * cumulativeValue;
+   
+    let multiplier = 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+        let val = multiplier * leftArray[i];
+        leftArray[i] = val;
+        multiplier = multiplier * nums[i];
     }
-    return output;
+    return leftArray;
 };
 
 
