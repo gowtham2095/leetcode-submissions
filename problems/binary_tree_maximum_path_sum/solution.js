edcaa41ee5b66a26385c99de05f8a412ec463057
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-03-28 06:59:52 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (javascript)

--- problems/binary_tree_maximum_path_sum/solution.js
+++ problems/binary_tree_maximum_path_sum/solution.js
@@ -16,17 +16,29 @@
 
 // When we return we do Math.max(left subtree and right subtree). Coz to be considered as a path for upper level root there can only be one subtree through which the path emerges.
 
-function traverseTree(root, result = {}) {
+
+// Inferences 
+
+// 1) I have to consider the whole left + right + root for max value
+// 2) But for path I have to include whatever is bigger.
+
+
+
+function traverseTree(root, result) {
     if (root) {
-        let leftSum = Math.max(traverseTree(root.left, result), 0);
-        let rightSum = Math.max(traverseTree(root.right, result), 0);
-        let totalSum = leftSum + rightSum + root.val;
-        result.max = Math.max(result.max, totalSum);
-        return root.val + Math.max(leftSum, rightSum);
+        let leftVal = Math.max(traverseTree(root.left, result), 0);
+        let rightVal = Math.max(traverseTree(root.right, result), 0);
+        let totalVal = leftVal + rightVal + root.val;
+        result.max = Math.max(result.max, totalVal);
+        return leftVal > rightVal ? leftVal + root.val : rightVal + root.val;
+        
     } else {
         return 0;
     }
-}
+} 
+
+
+
 
 var maxPathSum = function(root) {
     let result = {
@@ -36,6 +48,37 @@ var maxPathSum = function(root) {
     traverseTree(root, result);
     return result.max;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function traverseTree(root, result = {}) {
+//     if (root) {
+//         let leftSum = Math.max(traverseTree(root.left, result), 0);
+//         let rightSum = Math.max(traverseTree(root.right, result), 0);
+//         let totalSum = leftSum + rightSum + root.val;
+//         result.max = Math.max(result.max, totalSum);
+//         return root.val + Math.max(leftSum, rightSum);
+//     } else {
+//         return 0;
+//     }
+// }
+
+
 
 
 
