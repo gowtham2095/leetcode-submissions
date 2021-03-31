abb845ecab087579ed25829a5342edca5be01c6c
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-03-31 03:33:08 +0000

    Sync LeetCode submission - K Closest Points to Origin (javascript)

--- problems/k_closest_points_to_origin/solution.js
+++ problems/k_closest_points_to_origin/solution.js
@@ -11,15 +11,12 @@ function calculatedDistance(x, y) {
 var kClosest = function(points, k) {
     let result = [];
     let plane = [];
-    
     for (let i = 0; i < points.length; i++) {
         plane.push({val: calculatedDistance(points[i][0], points[i][1]), points: points[i]});
     }
-    
     plane.sort((a, b) => {
        return a.val - b.val;
     });
-    
     plane = plane.map((ele) => ele.points);
     return plane.slice(0, k);
 };
