91447ca75a4c29e5331cefab16d8820b833bba77
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-04 11:52:43 +0000

    Sync LeetCode submission - Wiggle Sort (javascript)

--- problems/wiggle_sort/solution.js
+++ problems/wiggle_sort/solution.js
@@ -3,17 +3,43 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-function swap(arr, i, j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+function swap(nums, i, j) {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+function wiggleWiggle(nums) {
+    for (let i = 1; i < nums.length; i++) {
+        if (i % 2) {
+            if (nums[i] < nums[i - 1]) {
+                swap(nums, i, i - 1);
+            }
+        } else {
+            if (nums[i] > nums[i - 1]) {
+                swap(nums, i, i - 1);
+            }
+        }
+    }
 }
 
 var wiggleSort = function(nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        if ((i % 2 == 0) ==  (nums[i] > nums[i+1])) {
-            swap(nums, i, i+1);
-        }
-    }
+    wiggleWiggle(nums);
     return nums;
 };
+
+
+
+
+// [3,5,2,1,6,4]
+
+
+//    3 < 5
+//    5 > 2
+//    2 < 1 ? nope so swap 3, 5, 1, 2
+//    2 > 6 ? nope so swap 3, 5, 1, 6, 2
+//    2 < 4 yes
+   
+   
+//    3, 5, 1, 6, 2, 4
+   
