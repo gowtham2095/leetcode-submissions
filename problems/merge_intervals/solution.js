21ddc512591b0e1e84ae19c28f1809f9fd16e9cb
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-09 16:33:24 +0000

    Sync LeetCode submission - Merge Intervals (javascript)

--- problems/merge_intervals/solution.js
+++ problems/merge_intervals/solution.js
@@ -4,26 +4,21 @@
  */
 
 function mergeIntervals(intervals) {
-    intervals.sort((a, b) => a[0] - b[0]);
-    let result = [];
-    result.push(intervals[0]);
+    let result = [intervals[0]];
     for (let i = 1; i < intervals.length; i++) {
-        let resultLength = result.length - 1;
-        if (intervals[i][0] <= result[resultLength][1] && intervals[i][1] >= result[resultLength][0]) {
-            let resultLatest = result.pop();
-            let left = Math.min(resultLatest[0], intervals[i][0]);
-            let right = Math.max(resultLatest[1], intervals[i][1]);
-            result.push([left, right]);
-        } else {
-            result.push(intervals[i]);
+        let lastResult = result[result.length - 1];
+        let left = intervals[i][0];
+        let right = intervals[i][1];
+        if (left <= lastResult[1] && right >= lastResult[0]) {
+            result.pop();
+            left = Math.min(lastResult[0], left);
+            right = Math.max(lastResult[1], right);
         }
+        result.push([left, right]);
     }
     return result;
 }
-
 var merge = function(intervals) {
+    intervals.sort((intervalA, intervalB) => intervalA[0] - intervalB[0]);
     return mergeIntervals(intervals);
 };
-
-
-
