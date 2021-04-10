2452c0ab9f811868f5c45a99477d3fbbc717e04c
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-10 07:03:26 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (javascript)

--- problems/kth_smallest_element_in_a_bst/solution.js
+++ problems/kth_smallest_element_in_a_bst/solution.js
@@ -14,14 +14,12 @@
 function findKthSmallest(root, k) {
    let stack = [];
    let node = root;
-   let list = [];
    while (node || stack.length != 0) {
        while(node) {
            stack.push(node);
            node = node.left;
        }
        let ele = stack.pop();
-       list.push(ele.val);
        k--;
        if (k == 0)
            return ele.val;
