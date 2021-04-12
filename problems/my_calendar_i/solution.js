7e3d936a268cb89fc32cb368813841c28e0ac608
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-12 06:36:11 +0000

    Sync LeetCode submission - My Calendar I (javascript)

--- problems/my_calendar_i/solution.js
+++ problems/my_calendar_i/solution.js
@@ -9,16 +9,12 @@ var MyCalendar = function() {
  * @return {boolean}
  */
 MyCalendar.prototype.book = function(start, end) {
-    
     for (let i = 0; i < this.events.length; i++) {
-        
         if (start < this.events[i][1] && end > this.events[i][0]) {
             return false;
         }
-        
     }
     this.events.push([start, end]);
-    this.events.sort((a, b) => a[0] - b[0]);
     return true;
 };
 
