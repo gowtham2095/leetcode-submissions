b020efdaccab8d7915f9f04980318caba465530f
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-23 13:28:29 +0000

    Sync LeetCode submission - Queue Reconstruction by Height (javascript)

--- problems/queue_reconstruction_by_height/solution.js
+++ problems/queue_reconstruction_by_height/solution.js
@@ -2,39 +2,16 @@
  * @param {number[][]} people
  * @return {number[][]}
  */
-
-
-function queueReconstruction(people) {
-    people.sort((current, next) => {
-       return current[0] == next[0] ? current[1] - next[1] : next[0] - current[0];
-    });
+function constructQueue(people) {
+    people.sort((a, b) => a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]);
     let result = [];
-    for (let i = 0; i < people.length; i++) {
+    result.push(people[0]);
+    for (let i = 1; i < people.length; i++) {
         result.splice(people[i][1], 0, people[i]);
     }
     return result;
 }
 
 var reconstructQueue = function(people) {
-    return queueReconstruction(people);
+    return constructQueue(people);
 };
-
-
-
-
-// [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
-
-
-// [[7,0], [5, 0], [6, 1], [7, 1], [5, 2], [4, 4]]
-
-    
-
-//      [5, 0], [7,0], [6, 1], [7, 1], [5, 2], [4, 4]
-
-     
-
-
-//     [5, 0], [7, 0] [6, 1], 
-
-//     min = 5
-//     max = 7
