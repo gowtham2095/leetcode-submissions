3b90605fe0ca7713598de5133ae4f12b821ae778
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-24 08:02:15 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (javascript)

--- problems/search_in_rotated_sorted_array/solution.js
+++ problems/search_in_rotated_sorted_array/solution.js
@@ -4,12 +4,63 @@
  * @return {number}
  */
 
-var findMin = function(nums) {
+// var findMin = function(nums) {
+//     let left = 0;
+//     let right = nums.length - 1;
+//     while (left < right) {
+//         let middle = Math.floor((left + right)/2);
+        
+//         if (nums[middle] > nums[right]) {
+//             left = middle + 1;
+//         } else {
+//             right = middle;
+//         }
+//     }
+//     return left;
+// };
+
+// var findInBinary = function(nums, left, right, target) {
+//    while(left <= right) {
+//        let middle = Math.floor((left + right)/2);
+//        if (nums[middle] == target) {
+//            return middle;
+//        } else if (nums[middle] > target) {
+//            right = middle - 1;
+//        } else {
+//            left = middle + 1;
+//        }
+//    }
+//    return -1;
+// }
+
+// var search = function(nums, target) {
+    
+//     let start = findMin(nums);
+//     if (nums[start] <= target && target <= nums[nums.length - 1]) {
+//         return findInBinary(nums, start, nums.length -1, target);
+//     } else {
+//         return findInBinary(nums, 0, start - 1, target);
+//     }
+// };
+
+// 4, 5, 6, 7, 0, 1, 2
+
+
+// [6,7,0,1,2, 4,5]
+//  l   r         r                   
+
+
+// 1, 3
+// 0  1
+
+
+// 0
+
+function findTheSplitPoint(nums) {
     let left = 0;
     let right = nums.length - 1;
-    while (left < right) {
-        let middle = Math.floor((left + right)/2);
-        
+    while(left < right) {
+        let middle = left + Math.floor((right - left)/2);
         if (nums[middle] > nums[right]) {
             left = middle + 1;
         } else {
@@ -17,53 +68,71 @@ var findMin = function(nums) {
         }
     }
     return left;
-};
-
-var findInBinary = function(nums, left, right, target) {
-   while(left <= right) {
-       let middle = Math.floor((left + right)/2);
-       if (nums[middle] == target) {
-           return middle;
-       } else if (nums[middle] > target) {
-           right = middle - 1;
-       } else {
-           left = middle + 1;
-       }
-   }
-   return -1;
 }
 
-var search = function(nums, target) {
-//     let left = 0;
-//     let right = nums.length - 1;
-    
-//     while (left <= right) { 
-//       let middle = Math.floor((left + right)/2);
-//       if (nums[middle] === target)
-//          return middle;
-//       else if (target <= nums[middle] && target > nums[left]) {
-//          right = middle - 1;
-//       } else if (target > nums[middle] && target <= nums[right]) {
-//          left  = middle + 1;
-//       } else {
-//         if (nums[left] >= nums[middle]) {
-//           right = middle - 1;
-//         } else {
-//           left = middle + 1;
-//         }
-//       }
-//     }
-
-//     return -1;
-    
-    let start = findMin(nums);
-    console.log(start);
-    console.log(nums[nums.length - 1]);
-    if (nums[start] <= target && target <= nums[nums.length - 1]) {
-        return findInBinary(nums, start, nums.length -1, target);
-    } else {
-        return findInBinary(nums, 0, start - 1, target);
+function binarySearch(arr, left, right, target) {
+    while(left <= right) {
+        let middle = left + Math.floor((right - left)/2);
+        if (arr[middle] == target)
+            return middle;
+        else if (arr[middle] < target) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
     }
-};
+    return - 1;
+}
 
-// 4, 5, 6, 7, 0, 1, 2
+
+function binarySearchOnePass(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right) {
+        let middle = left + Math.floor((right - left)/2);
+        if (nums[middle] == target)
+            return middle;
+        else if (nums[middle] >= nums[left]) {
+            if (target <= nums[middle] && target >= nums[left]) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        } else {
+            if (target <= nums[right] && target >= nums[middle]) {
+                left = middle;
+            } else {
+                right = middle - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+
+function search(nums, target) {
+    // let splitPoint = findTheSplitPoint(nums);
+    // let res1 = binarySearch(nums, 0, splitPoint - 1, target);
+    // let res2 = binarySearch(nums, splitPoint, nums.length, target);
+    // return (res1 != -1) ? res1 : res2;
+
+    return binarySearchOnePass(nums, target);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
