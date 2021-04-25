17209c613c6fce770d6a6c8d8eccb07dd6e8b4ce
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-25 15:11:28 +0000

    Sync LeetCode submission - Permutations (javascript)

---
+++ problems/permutations/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+
+// 17 minutes
+
+function permuteList(nums, result, comb, set) {
+    if (comb.length == nums.length) {
+        result.push([...comb]);
+        return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (!set.has(nums[i])) {
+            comb.push(nums[i]);
+            set.add(nums[i]);
+            permuteList(nums, result, comb, set);
+            comb.pop();
+            set.delete(nums[i]);
+        }
+    }
+}
+var permute = function(nums) {
+    let result = [];
+    permuteList(nums, result, [], new Set());
+    return result;
+};
+
