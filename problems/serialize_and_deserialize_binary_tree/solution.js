34df41e3e32b6bc71d737010336eebca9f7a0a79
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-04-25 13:11:28 +0000

    Sync LeetCode submission - Serialize and Deserialize Binary Tree (javascript)

--- problems/serialize_and_deserialize_binary_tree/solution.js
+++ problems/serialize_and_deserialize_binary_tree/solution.js
@@ -13,54 +13,45 @@
  * @return {string}
  */
 
-function preOrder(root, str = '') {
-    function buildString(node) {
-        if (node == null) {
-            str += 'e' + ','
-        } else {
-            str += node.val + ','
-            buildString(node.left, str);
-            buildString(node.right, str);
-        }
+function dfsPreorder(root, result) {
+    if (root) {
+        result.push(root.val);
+        dfsPreorder(root.left, result);
+        dfsPreorder(root.right, result);
+    } else {
+        result.push('e');
     }
-    buildString(root);
-    return str;
 }
 
-
 var serialize = function(root) {
-    return preOrder(root);
+    let result = [];
+    dfsPreorder(root, result);
+    return result.join();
 };
 
+function formTree(treeArr) {
+    let data = treeArr.shift();
+    let root = null;
+    if (data != 'e') {
+        root = new TreeNode(data);
+        root.left = formTree(treeArr);
+        root.right = formTree(treeArr);
+    }
+    return root;
+}
 /**
  * Decodes your encoded data to tree.
  *
  * @param {string} data
  * @return {TreeNode}
  */
-
-function parse(str) {
-    let dataSplit = str.split(',');
-    function buildTree() {
-        let node = dataSplit.shift();
-        if (node == 'e') {
-            return null;
-        } else {
-            let newNode = new TreeNode(node);
-            newNode.left = buildTree();
-            newNode.right = buildTree();
-            return newNode;
-        }
-    }
-    return buildTree();
-}
-
 var deserialize = function(data) {
-    return parse(data);
+    let treeArr = data.split(',');
+    let root = formTree(treeArr);
+    return root;
 };
 
 /**
  * Your functions will be called as such:
  * deserialize(serialize(root));
  */
-
