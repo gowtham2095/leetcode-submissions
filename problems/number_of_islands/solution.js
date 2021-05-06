b1e5310aec4d44e86458a9c7de56fdd542b78f1b
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-06 18:48:10 +0000

    Sync LeetCode submission - Number of Islands (javascript)

--- problems/number_of_islands/solution.js
+++ problems/number_of_islands/solution.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 
-function doDfs(grid, i, j) {
-    if (i > grid.length -1 || j > grid[0].length - 1 || j < 0 || i < 0)
+function dfs(grid, i, j) {
+    if ( i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == '0')
         return;
-    else if (grid[i][j] == '0')  {
-        return;
-    } else {
-        grid[i][j] = '0';
-    }
-    doDfs(grid, i + 1, j);
-    doDfs(grid, i, j + 1);
-    doDfs(grid, i - 1, j);
-    doDfs(grid, i, j - 1);
+    grid[i][j] = '0';
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j - 1);
 }
 
 function countIslands(grid) {
@@ -23,7 +19,7 @@ function countIslands(grid) {
         for (let j = 0; j < grid[0].length; j++) {
             if (grid[i][j] == '1') {
                 count++;
-                doDfs(grid, i, j);
+                dfs(grid, i, j);
             }
         }
     }
@@ -33,50 +29,3 @@ function countIslands(grid) {
 var numIslands = function(grid) {
     return countIslands(grid);
 };
-
-
-[["1","1","0","0","0"],
- ["1","1","0","0","0"],
- ["0","0","1","0","0"],
- ["0","0","0","1","1"]]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function dfs(grid, i, j) {
-    
-//     if (i >= grid.length || i < 0 || j < 0 || j >= grid[0].length || grid[i] && grid[i][j] === '0')
-//         return;
-//     grid[i][j] = '0';
-//     dfs(grid, i, j + 1);
-//     dfs(grid, i + 1, j);
-//     dfs(grid, i, j - 1);
-//     dfs(grid, i - 1 , j);
-// }
-
-// function countIslands(grid) {
-//     let count = 0;
-//     for(let i = 0; i < grid.length; i++) {
-//         for (let j = 0; j < grid[0].length; j++) {
-//             if (grid[i][j] === '1') {
-//                 count++;
-//                 dfs(grid, i, j)
-//             }
-//         }
-//     }
-//     return count;
-// }
