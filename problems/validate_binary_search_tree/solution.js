fa9a2e234cf009544bcca044a77ad89209146048
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-08 07:43:40 +0000

    Sync LeetCode submission - Validate Binary Search Tree (javascript)

--- problems/validate_binary_search_tree/solution.js
+++ problems/validate_binary_search_tree/solution.js
@@ -15,9 +15,6 @@ function isValid(root, lowValue, highValue) {
     if (root == null) {
         return true;
     }
-    // if ( lowValue >= root.val || highValue <= root.val) {
-    //    return false;
-    // }
     return lowValue < root.val && highValue > root.val && isValid(root.left, lowValue, root.val) && isValid(root.right, root.val, highValue);
 }
 
