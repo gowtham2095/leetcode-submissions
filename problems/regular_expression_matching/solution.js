ad2d18ceedd5258c2e71a87d77512ba8311ad679
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-08 07:45:25 +0000

    Sync LeetCode submission - Regular Expression Matching (javascript)

--- problems/regular_expression_matching/solution.js
+++ problems/regular_expression_matching/solution.js
@@ -4,53 +4,20 @@
  * @return {boolean}
  */
 
-function isSAndPEqual(s, p, i, j) {
-    return s[i] == p[j] || s[i] != undefined && p[j] == '.';    
+function doStringsMatch(s, p, i, j, map) {
+    if (map[i + '#' + j])
+        return map[i + '#' + j];
+    if (j == p.length)
+        return i == s.length;
+    let first = s[i] != undefined && (p[j] == s[i] || p[j] == '.');
+    if (j <= p.length - 2 && p[j + 1] == '*') {
+        return map[i + '#' + j] = (doStringsMatch(s, p, i, j + 2, map) || (first && doStringsMatch(s, p, i + 1, j, map)));
+    }
+    return  map[i + '#' + j] = (first && doStringsMatch(s, p, i + 1, j + 1, map));
 }
 
-function isMatchRegex(s, p, i, j) {
-    if (i >= s.length && j >= p.length)
-        return true;
-    if (p[j + 1] == '*') {
-        return isMatchRegex(s, p, i, j + 2) || isSAndPEqual(s, p, i, j) && isMatchRegex(s, p, i + 1, j);
-    } else if (isSAndPEqual(s, p, i, j)) {
-        return isMatchRegex(s, p, i + 1, j + 1);
-    } else {
-        return false;
-    }
-}
 
 var isMatch = function(s, p) {
-    return isMatchRegex(s, p, 0, 0);
+    let map = {};
+    return doStringsMatch(s, p, 0, 0, map);
 };
-
-""
-".*c"
-
-
-
-
-// if characters match then move forward
-
-
-
-// if (i == s.length and j == p.length)
-//     return true;
-
-
-// p[i] =='.' || s[i] == p[j]
-
-//     return s[i + 1] , p[j + 1]
-
-
-
-// // '' aa
-//       a*
-
-// // a* 
-
-
-
-// a
-// c*
-    
