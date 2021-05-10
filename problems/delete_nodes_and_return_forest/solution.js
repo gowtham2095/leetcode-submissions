775f8609dc833d45569c8ee0169f0e777853811e
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-10 18:53:49 +0000

    Sync LeetCode submission - Delete Nodes And Return Forest (javascript)

--- problems/delete_nodes_and_return_forest/solution.js
+++ problems/delete_nodes_and_return_forest/solution.js
@@ -12,63 +12,83 @@
  * @return {TreeNode[]}
  */
 
- 
-function dfs(root, deleteSet, result, parent, direction) {
+
+
+
+//                         1
+        
+//             2                       3
+
+
+//         4       5               6       7
+
+//     78      31
+
+// 3, 4, 2
+function deleteTheNodes(root, result, deleteSet) {
     if (root) {
-        dfs(root.left, deleteSet, result, root, 'left');
-        dfs(root.right, deleteSet, result, root, 'right');
+        root.left = deleteTheNodes(root.left, result, deleteSet);
+        root.right = deleteTheNodes(root.right, result, deleteSet);
         if (deleteSet.has(root.val)) {
             if (root.left) {
+                // if (!deleteSet.has(root.left)) {
                 result.push(root.left);
+                // }
             }
             if (root.right) {
-                result.push(root.right)
+                // if (!deleteSet.has(root.right)) {
+                result.push(root.right);
+                // }
             }
-            if (parent && direction == 'left') {
-                parent.left = null;
-            }
-            if (parent && direction == 'right') {
-                parent.right = null;
-            }
+            return null;
+        } else {
+            return root;
         }
+
+
+    } else {
+        return null;
     }
 }
-
-function forestList(root, to_delete) {
-    let set = new Set()
-    for(let i = 0; i < to_delete.length; i++) {
-        set.add(to_delete[i]);
-    }
-    let result = [];
-    dfs(root, set, result, null);
-    if (!set.has(root.val))
-        result.push(root);
-    return result;
-}
-
 
 
 var delNodes = function(root, to_delete) {
-    return forestList(root, to_delete);
+    let toDeleteNodes = new Set(to_delete);
+    let result = [];
+    deleteTheNodes(root, result, toDeleteNodes);
+    if (!toDeleteNodes.has(root.val)) {
+        result.push(root);
+    }
+    return result;
 };
 
 
+//             1
+//         2       4
+
+//     null  3
+
+
+[1,2,3,null,null,null,4]
+[2,1]
+
+
+        
 
 
 
 
-//                 1
-
-//         2               3 
+// 78, 31
 
 
-//     4       5        6      7
-                                    
-                            // 98        8
+// if the node to be deleted is found 
 
+// then add the left and right subtrees if present
 
+// while up in the root if left or right is the root to be deleted then make it as null and move up
 
+// result 
 
-
+// [4, 
 
 
