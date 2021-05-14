17070f175284f4bfcacc9d2f8af10684fddd29dd
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-14 13:22:44 +0000

    Sync LeetCode submission - Sentence Similarity (javascript)

--- problems/sentence_similarity/solution.js
+++ problems/sentence_similarity/solution.js
@@ -5,47 +5,73 @@
  * @return {boolean}
  */
 
-function constructSets(pairs, map) {
-    pairs.forEach((pair) => {
-       let [a, b] = pair;
-       let set = map.get(a)
-       if (!set) {
-           set = new Set();
-           map.set(a, set);
-       }
-        set.add(b);
-        set = map.get(b)
-        if (!set) {
-          set = new Set();
-          map.set(b, set);
-        }
-        set.add(a);
-    });
+
+function areTheSimilar(sentence1, sentence2, similarityMap) {
+    if (sentence1.length != sentence2.length)
+        return false;
+    for (let i = 0; i < sentence1.length; i++) {
+        let word1 = sentence1[i];
+        let word2 = sentence2[i];
+        let similaritySet = similarityMap[word1];
+        if (!similaritySet.has(word2))
+            return false;  
+    }
+    return true;
 }
 
 var areSentencesSimilar = function(sentence1, sentence2, similarPairs) {
-    if (sentence1.length != sentence2.length) {
-        return false;
-    }
-    let map = new Map();
-    constructSets(similarPairs, map);
-    for (let i = 0; i < sentence1.length; i++) {
-        let a = sentence1[i];
-        let b = sentence2[i];
-        if (a != b) { 
-            let set1 = map.get(a);
-            let set2 = map.get(b);
-            if (!set1 || !set2) {
-                return false;
-            }
-            if (!set1.has(b) || !set2.has(a)) {
-                return false
-            }
+    let similaritySet = new Set();
+    let similarityMap = {};
+    
+    for (let word of sentence1) {
+        let currentSet = similarityMap[word];
+        if (currentSet == undefined) {
+            similarityMap[word] = new Set()
+            similarityMap[word].add(word);
         }
-    }  
-    return true; 
+    }
+    for (let word of sentence2) {
+        let currentSet = similarityMap[word];
+        if (currentSet == undefined) {
+            similarityMap[word] = new Set()
+            similarityMap[word].add(word);
+        }
+    }
+    for (let pair of similarPairs) {
+        let [firstpair, secondpair] = pair;
+        let getSet = similarityMap[firstpair];
+        if (getSet != undefined) {
+            getSet.add(secondpair);
+        }
+        getSet = similarityMap[secondpair];
+        if (getSet != undefined) {
+            getSet.add(firstpair);
+        }
+    }
+    return areTheSimilar(sentence1, sentence2, similarityMap);
 };
 
+
+
+
+
+// sentence 1 -> ["great","acting","skills"]
+
+// sentence2 = ["fine","drama","talent"]
+
+// similarPairs = [["great","fine"],["drama","acting"],["skills","talent"]]
+
+
+
+// ["great"]
+
+// great: new set(great, fine)
+
+// sentence1 = ["great"]
+
+// sentence2 = ["doubleplus","good"]
+
+// [["great","doubleplus"]]
 
 
 
