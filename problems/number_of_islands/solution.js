5097c15babf93241041f1e2cc376cacc97ee63e3
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-17 16:54:17 +0000

    Sync LeetCode submission - Number of Islands (javascript)

--- problems/number_of_islands/solution.js
+++ problems/number_of_islands/solution.js
@@ -3,29 +3,25 @@
  * @return {number}
  */
 
-function dfs(grid, i, j) {
-    if ( i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == '0')
+function countIslands(grid, row, col) {
+    if (row < 0 || col < 0 || row > grid.length - 1 || col > grid[0].length -1 || grid[row][col] == '0')
         return;
-    grid[i][j] = '0';
-    dfs(grid, i + 1, j);
-    dfs(grid, i, j + 1);
-    dfs(grid, i - 1, j);
-    dfs(grid, i, j - 1);
+    grid[row][col] = '0';
+    countIslands(grid, row + 1, col);
+    countIslands(grid, row - 1, col);
+    countIslands(grid, row, col + 1);
+    countIslands(grid, row, col - 1);
 }
 
-function countIslands(grid) {
+var numIslands = function(grid) {
     let count = 0;
     for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
+        for(let j = 0; j < grid[0].length; j++) {
             if (grid[i][j] == '1') {
                 count++;
-                dfs(grid, i, j);
+                countIslands(grid, i, j);
             }
         }
     }
     return count;
-}
-
-var numIslands = function(grid) {
-    return countIslands(grid);
 };
