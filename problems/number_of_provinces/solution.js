bb6c08db034be613e166de297966610d3e5ca3e6
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-17 04:47:35 +0000

    Sync LeetCode submission - Number of Provinces (javascript)

--- problems/number_of_provinces/solution.js
+++ problems/number_of_provinces/solution.js
@@ -46,13 +46,3 @@ function findNumberOfProvinces(isConnected) {
 var findCircleNum = function(isConnected) {
     return findNumberOfProvinces(isConnected);
 };
-//   0 1 2 3
-// 0[1,0,0,1],
-// 1[0,1,1,0],
-// 2[0,1,1,1],
-// 3[1,0,1,1]
-
-//  0. 1. 2
-// 0[[1,1,0],
-// 1[1,1,0],
-// 2[0,0,1]]
