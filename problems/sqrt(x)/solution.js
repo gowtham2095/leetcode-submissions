46c75dbd4f51b221646b32d9d81d6d483a534e3b
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-30 12:44:25 +0000

    Sync LeetCode submission - Sqrt(x) (javascript)

--- problems/sqrt(x)/solution.js
+++ problems/sqrt(x)/solution.js
@@ -6,16 +6,18 @@
 
 
 function findSqrt(x) {
-    let left = 1;
-    let right = x + 1;
-    
-    while (left < right) {
-        let middle = left + Math.floor((right - left)/2);
-        
-        if (middle * middle > x)
-            right = middle;
-        else 
+    let left = 0;
+    let right = x;
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (middle * middle == x) {
+            return middle
+        }
+        else if (middle * middle > x) {
+            right = middle - 1;
+        } else {
             left = middle + 1;
+        }
     }
     return left - 1;
 }
@@ -23,4 +25,23 @@ function findSqrt(x) {
 var mySqrt = function(x) {
     return findSqrt(x);
 };
+
+
+
+// 2 3
+
+// 8
+
+
+
+// 4 
+
+
+// 16 
+
+
+// 0 4
+
+
+
 
