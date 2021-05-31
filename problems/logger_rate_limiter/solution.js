3a9b89d3391c49316c1e0d22af87b39b5b5ec8e1
Hariharan Sampath <hariharan@Hariharans-MacBook-Air.local>
2021-05-31 06:30:30 +0000

    Sync LeetCode submission - Logger Rate Limiter (javascript)

--- problems/logger_rate_limiter/solution.js
+++ problems/logger_rate_limiter/solution.js
@@ -1,44 +1,8 @@
 /**
  * Initialize your data structure here.
  */
-
-function MinHeap() {
-    this.list = [{val: -Infinity, key: '-1'}];
-    this.size = 0;
-}
-
-MinHeap.prototype.insert = function(key, val) {
-    for (var i = ++this.size; val < this.list[Math.floor(i/2)].val; i= Math.floor(i/2)) {
-        this.list[i] = this.list[Math.floor(i/2)];
-    }
-    this.list[i] = {key, val};
-}
-
-
-
-MinHeap.prototype.remove = function() {
-    let first = this.list[0];
-    let last = this.list[this.size--];
-    let child;
-    for (var i = 1; 2 * i <= this.size; i = child) {
-        child = 2 * i;
-        
-        if (child + 1 <= this.size && this.list[child + 1].val < this.list[child].val)
-            child = child + 1;
-        
-        if (this.list[child].val < last.val) {
-            this.list[i] = this.list[child];
-        } else {
-            break;
-        }
-    }
-    this.list[i] = last;
-    return first;
-}
-
 var Logger = function() {
-    this.mapOfLogger = new Map();
-    this.heap = new MinHeap();
+    this.myMap = new Map();
 };
 
 /**
@@ -49,33 +13,19 @@ var Logger = function() {
  * @param {string} message
  * @return {boolean}
  */
-
-function canInsertTheLog(timestamp, message, heap, map) {
-    // console.log(map, message, timestamp, heap)
-    // console.log(timestamp - map.get(message));
-    if (map.get(message) != undefined && timestamp - map.get(message) < 10) {
-        return false;
-    }
-    let heapTop = heap.list[1];
-    while(heap.size != 0 && timestamp - heapTop.val > 10) {
-        // console.log(heap.size)
-        let ele = heap.remove();
-        map.delete(ele.key);
-        heapTop = heap.list[1];
-    }
-    heap.insert(message, timestamp);
-    map.set(message, timestamp);
-    // console.log(heap.list)
-    return true;
-}
-
 Logger.prototype.shouldPrintMessage = function(timestamp, message) {
-    if (this.heap.size === 0) {
-        this.heap.insert(message, timestamp);
-        this.mapOfLogger.set(message, timestamp);
+    if (!this.myMap.has(message) ) {
+        this.myMap.set(message, Number(timestamp));
         return true;
+    } else {
+        let timeForMessage = this.myMap.get(message);
+        if (Number(timestamp) - timeForMessage >= 10) {
+           this.myMap.set(message, timestamp);
+           return true;
+        } else {
+            return false;
+        }
     }
-    return canInsertTheLog(timestamp, message, this.heap, this.mapOfLogger);
 };
 
 /** 
@@ -83,45 +33,3 @@ Logger.prototype.shouldPrintMessage = function(timestamp, message) {
  * var obj = new Logger()
  * var param_1 = obj.shouldPrintMessage(timestamp,message)
  */
-
-
-
-
-// [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11, "foo"]
-// true        true        false       false       false        true
-
-
-// [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "jhsdf"], [11, "sdfjn"]
-
-// Maintain a hashmap it would work
-
-// "foo": 1
-// "bar": 2
-// "foo"
-
-
-
-
-//                         1
-
-//                 2               3  
-
-            
-        
-
-
-// i) If the value is not present in my hashmap push it to the heap then return true
-
-// ii) if the 
-
-
-
-
-
-
-// For every incoming log
-
-//     i) If there is nothing in the heap I will push it to the heap
-//     ii) If there is a value in the heap and the difference between the root and the value is lesser than or equal to 10 push it to the heap if the value is not present in the map already - else return false
-    
-//     iii) If the value is greater than 10 then keep removing the heap top till the difference is less than 10 and push it to the heap
